bcb17e5ec52387bd7be0062762ba1ad7db1fa072
Pephma <103604628+Pephma@users.noreply.github.com>
2023-06-10 01:46:39 -0300

    Update shodan_test.py

--- shodan/tests/shodan_test.py
+++ shodan/tests/shodan_test.py
@@ -6,7 +6,7 @@ import pathlib
 import sys
 import time
 
-import shodan  # Import the Shodan module you created
+import shodan_script
 
 
 CWD = pathlib.Path(os.getcwd())
@@ -17,26 +17,33 @@ SHODAN_MOD_PATH = CWD.parent
 def callback(label: str, success: bool) -> None:
     logging.info("Callback for %s running, success=%s", label, success)
 
+def test_shodan(s, taskcfg):    
+    commands_shodan = ["ip_address","python","./shodan2.py",taskcfg.ip_address]
+
+    logging.info("RUNNING TEST_SHODAN")
+    s.enqueue(taskcfg, commands_shodan)
+    logging.info("Task submitted")
+    s.shutdown()
+
 
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting Shodan module")
-    cfg = shodan.ShodanConfig(
+    cfg = shodan_script.ShodanConfig(
         pathlib.Path(SHODAN_MOD_PATH / "dev/shodan-credentials"),
         pathlib.Path(SHODAN_MOD_PATH / "data"),
         docker_image="shodan-image",
         docker_poll_interval=1.0,
     )
     logging.info("Shodan module started")
-    s = shodan.Shodan(cfg, callback)
+    s = shodan_script.Shodan(cfg, callback)
     logging.info("Submitting task to Shodan module")
-    taskcfg = shodan.ShodanTask(
+    taskcfg = shodan_script.ShodanTask(
         time.strftime("shodan-%Y%m%d-%H%M%S"),
         "ip_address"
     )
-    s.enqueue(taskcfg)
-    logging.info("Task submitted")
-    s.shutdown()
+
+    test_shodan(s, taskcfg)
 
 
 if __name__ == "__main__":
