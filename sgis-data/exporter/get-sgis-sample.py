13d2ed199ba256f352aa5d8841acb88558e21822
Italo Cunha <cunha@dcc.ufmg.br>
2024-10-19 19:10:27 -0300

    feat: anonymize IPs other fields

--- sgis-data/exporter/get-sgis-sample.py
+++ sgis-data/exporter/get-sgis-sample.py
@@ -7,8 +7,10 @@ import os
 import pathlib
 import sys
 import time
+from typing import Any
 
 import psycopg2
+import sqlparse
 
 DB_BATCH_SIZE = int(os.getenv("DB_BATCH_SIZE", 1000))
 DB_NAME = os.getenv("DB_NAME", "fix")
@@ -18,7 +20,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", 5432)
 
 
-def generate_queries(csvfp: pathlib.Path) -> list[dict[str, str]]:
+def generate_queries(csvfp: pathlib.Path) -> list[dict[str, Any]]:
     queries = []
     current_table = None
     gathered_columns = []
@@ -83,8 +85,51 @@ def create_parser():
         help="Path to output directory [sgis-sample-%Y%m%d]",
         default=None,
     )
+    parser.add_argument(
+        "--test",
+        dest="testing",
+        action="store_const",
+        const=True,
+        required=False,
+        help="Perform a test run",
+        default=False,
+    )
 
     return parser
+
+
+def get_cursor(testing: bool, gathered_columns: list[str], connection):
+    if testing:
+        assert connection is None
+        return TestCursor(len(gathered_columns))
+    connection = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        host=DB_HOST,
+        port=DB_PORT,
+    )
+    return connection.cursor()
+
+
+class TestCursor:
+    def __init__(self, ncols: int):
+        self.ncols = ncols
+        self.fetched = False
+
+    def execute(self, query: str) -> None:
+        parsed = sqlparse.parse(query)
+        if not parsed:
+            raise RuntimeError(f"Generated an invalid SQL query: {query}")
+
+    def fetchmany(self, _batchsz: int) -> list[tuple]:
+        if self.fetched:
+            return []
+        self.fetched = True
+        return [tuple("0" for _ in range(self.ncols))] * 10
+
+    def close(self):
+        pass
 
 
 def main():
@@ -93,51 +138,48 @@ def main():
     parser = create_parser()
     args = parser.parse_args()
 
-    if DB_PASS == "fix" and False:
+    if DB_PASS == "fix" and not args.testing:
         logging.error("DB authentication variables not configured")
         logging.info("Please set DB auth variables at the beginning of the script")
-        logging.info(
-            "Alternatively, set the DB_NAME, DB_USER, DB_PASS, DB_HOST, and DB_PORT environment variables"
-        )
+        logging.info("Or set the DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT env vars")
         sys.exit(1)
+    logging.info("DB_USER %s", DB_USER)
 
     if args.outdir is None:
         args.outdir = pathlib.Path(time.strftime("sgis-sample-%Y%m%d", time.gmtime()))
     os.makedirs(args.outdir, exist_ok=True)
-
     logging.info("Saving results to %s", args.outdir)
+
     queries = generate_queries(args.inputfp)
 
-    logging.info("DB_USER %s", DB_USER)
-
-    connection = psycopg2.connect(
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASS,
-        host=DB_HOST,
-        port=DB_PORT,
-    )
-    cursor = connection.cursor()
+    connection: Any = None
+    if not args.testing:
+        connection = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS,
+            host=DB_HOST,
+            port=DB_PORT,
+        )
 
     for query in queries:
-        cursor.execute(query["sql"])
         table_name = query["table"]
         gathered_columns = query["columns"]
         colstr = ",".join(gathered_columns)
-
+        cursor = get_cursor(args.testing, gathered_columns, connection)
+        cursor.execute(query["sql"])
         with open(args.outdir / f"{table_name}.sql", "w", encoding="utf8") as outfd:
             while True:
                 rows = cursor.fetchmany(DB_BATCH_SIZE)
                 if not rows:
                     break
-                # rows = [tuple("0" for _ in range(len(gathered_columns)))] * 10
                 for row in rows:
                     stmt = f"INSERT INTO {table_name} ({colstr}) VALUES {row};\n"
                     outfd.write(stmt)
-                # break
+        cursor.close()
 
-    cursor.close()
-    connection.close()
+    if not args.testing:
+        connection.close()
 
 
 if __name__ == "__main__":
