90537d21ccb7032eb421886042a69dd9c6940108
Sacramento-20 <sacramento.15@hotmail.com>
2024-12-23 14:56:28 -0300

    update: refactor main.py

--- Authentication_ZAP_GT_CRIVO/framework/main.py
+++ Authentication_ZAP_GT_CRIVO/framework/main.py
@@ -1,38 +1,29 @@
-from context.context import build_yaml, update_jobs
-from user_data.user_data import User
-from urls_login import urls_login
-from elements.elements import filtered_dict
-from keywords import keywords
-from autentication import autentication
-from params import params
+import os
+import logging
+import re
 import time
+from pathlib import Path
+
+from ruamel.yaml import YAML
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
-import os
 from zapv2 import ZAPv2
-from ruamel.yaml import YAML
-from pathlib import Path
-import re
 
+from autentication import autentication
+from context.context import build_yaml, update_jobs
+from keywords.elements import filtered_dict
+from keywords.keywords import RegexSets, parameter_types_found
+from params import params
+from urls_login import urls_login
+from user_data.user_data import Configuration
 
-DIRECTORY_PATH = "/shared_data/input_config"
+"""
+The default time is used to wit while selenium or zap is working, this delay is a reng sefety to wait a frameowrk to finish the work. 
+"""
+DEFAULT_TIME = 3
 
-
-def wait_for_directory(directory_path=DIRECTORY_PATH, interval=1):
-    directory_path = Path(directory_path)
-
-    print(f"Aguardando o diretório ou arquivos dentro de: {directory_path}")
-    while not (directory_path.exists() and any(directory_path.iterdir())):
-        time.sleep(interval)
-
-    if directory_path.exists():
-        if any(directory_path.iterdir()):
-            print(f"Arquivos encontrados no diretório: {directory_path}")
-            return
-        else:
-            print(f"Diretório encontrado, mas está vazio: {directory_path}")
-            return
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 
 def main(file_path):
@@ -41,17 +32,22 @@ def main(file_path):
 
     json_data = file_path.read_text()
 
-    # Validando os dados JSON usando model_validate_json
-    user = User.model_validate_json(json_data)
+    user = Configuration.model_validate_json(json_data)
 
-    print(user)
-    print("\n")
+    logging.info("User configuration loaded successfully.")
 
-    # --------------------------------------------------- BROWSER CONFIG ---------------------------------------------------
+    regex_sets = RegexSets()
+
+    invalid_values_urls = regex_sets.invalid_values_urls
+    url_words = regex_sets.url_words
+
     FIREFOX = os.getenv("FIREFOX")
     ZAP_API_KEY = os.getenv("ZAP_API_KEY")
+    if FIREFOX is None or ZAP_API_KEY is None:
+        raise EnvironmentError("FIREFOX e ZAP_API_KEY devem estar definidos nas variáveis de ambiente.")
+
     ZAP_PROXY_ADDRESS = os.getenv("ZAP_PROXY_ADDRESS")
-    ZAP_PROXY_PORT = 8080
+    ZAP_PROXY_PORT = int(os.getenv("ZAP_PROXY_PORT"))
 
     zap = ZAPv2(
         apikey=ZAP_API_KEY,
@@ -60,10 +56,10 @@ def main(file_path):
             "https": f"http://{ZAP_PROXY_ADDRESS}:{ZAP_PROXY_PORT}",
         },
     )
-    proxyServerUrl = f"{ZAP_PROXY_ADDRESS}:{ZAP_PROXY_PORT}"
+    proxy_server_url = f"{ZAP_PROXY_ADDRESS}:{ZAP_PROXY_PORT}"
     firefox_options = webdriver.FirefoxOptions()
     firefox_options.add_argument("--ignore-certificate-errors")
-    firefox_options.add_argument(f"--proxy-server={proxyServerUrl}")
+    firefox_options.add_argument(f"--proxy-server={proxy_server_url}")
     firefox_options.add_argument("--headless")
 
     s = Service(FIREFOX)
@@ -77,29 +73,12 @@ def main(file_path):
 
     firefox_options.profile = firefox_profile
 
-    # ---------------------- BROWSER CONFIG ----------------------
-
-    """
-    Criar uma flag que executa determinado trecho de codigo
-    se foi passado a url de login e caso contrário, executa
-    outro trecho de codigo.
-    """
-
-    """
-    0 - url de login não foi passada
-    1 - url de login foi passada
-
-    Valor será setado ao verificar se a estrutura 
-    em user_data é vazia ou não.
-    """
     if user.url_login == "":
         FLAG_LOGIN = 0
-        print("url de login não foi setada")
+        logging.info("url de login não foi setada")
     else:
         FLAG_LOGIN = 1
-        print("url de login setada")
-
-    DEFAULT_TIME = 3
+        logging.info("url de login setada")
 
     zap.core.new_session(overwrite=True)
 
@@ -108,81 +87,36 @@ def main(file_path):
     driver = webdriver.Firefox(service=s, options=firefox_options)
     if FLAG_LOGIN:
         filtered_dict(driver, user.url_login, struct_login)
-
         time.sleep(DEFAULT_TIME)
 
-    print(struct_login)
     if FLAG_LOGIN == 0:
-        # Primeira url será utilizada para realizar o crawler na página com o spider
-        driver.get(user.url[0])
-
-        time.sleep(DEFAULT_TIME)
-        scanid = zap.spider.scan(user.url[0])
-
-        time.sleep(DEFAULT_TIME)
-        while int(zap.spider.status(scanid)) < 100:
-            # Loop until the spider has finished
-            print("Spider progress %: {}".format(zap.spider.status(scanid)))
-            time.sleep(DEFAULT_TIME)
-
-        print("Spider completed")
-
-        driver.quit()
-
-        urls_found = zap.core.urls()
-
-        print(urls_found)
-        general_results = urls_login.find_urls_login(urls_found, keywords.url_words)
-        print(general_results)
-        evidences_urls_login = []
-        for url in general_results:
-            if not any(
-                re.search(pattern, url) for pattern in keywords.invalid_values_urls
-            ):  # match das palavras do array (regex group)
-                evidences_urls_login.append(url)
-
-        print(evidences_urls_login)
-        for evidence in evidences_urls_login:
-            driver = webdriver.Firefox(service=s, options=firefox_options)
-            filtered_dict(driver, evidence, struct_login)
-
-        print(struct_login)
-
-    print(len(struct_login))
-
-    # Autenticação
-    "-----------------------------------------------------------------------------------------------------------------------------------"
+        struct_login = spider_scan(driver, zap, user, urls_login, url_words, invalid_values_urls, s, firefox_options, struct_login)
+    
+    logging.info("Struct login: %s", struct_login)
 
     zap.core.new_session(overwrite=True)
 
-    # Inicializar o driver do Selenium
-    # Criar função
-    # conteudo da struct login
     driver = webdriver.Firefox(service=s, options=firefox_options)
     if len(struct_login) == 1:
         url_authentication = list(struct_login[0].keys())[0]
         driver.get(url_authentication)
-    elif len(struct_login) > 1:
-        pass
     else:
-        # lançar assert
-        pass
+        raise ValueError("Failed to create the list of URLs: struct_login does not contain exactly one element.")
 
     time.sleep(DEFAULT_TIME)
 
     for authentication_dictionary in struct_login:
         for _, elements in authentication_dictionary.items():
-            for type, field in elements.items():
+            for element_type, field in elements.items():
                 login_element, password_element = field
-                print(login_element, password_element)
-                # Encontrar os campos de login e senha (mudar)
+                logging.info(login_element, password_element)
                 username_field = autentication.find_element_by_attribute(
-                    driver, type, login_element
+                    driver, element_type, login_element
                 )
                 if username_field is None:
                     continue  # Se não encontrou o campo de username, pula para o próximo elemento
                 password_field = autentication.find_element_by_attribute(
-                    driver, type, password_element
+                    driver, element_type, password_element
                 )
                 if password_field is None:
                     continue  # Se não encontrou o campo de password, pula para o próximo elemento
@@ -192,55 +126,38 @@ def main(file_path):
 
                 time.sleep(DEFAULT_TIME)
 
-                # Tentar enviar o formulário
                 try:
                     password_field.send_keys(Keys.RETURN)
 
-                    time.sleep(1)
-                    autentication.validate_by_attribute(driver, type, login_element)
-                except Exception:
-                    pass
+                    time.sleep(DEFAULT_TIME)
+                    autentication.validate_by_attribute(driver, element_type, login_element)
+                except Exception as e:
+                    raise RuntimeError(f"Failed to submit the login form: {e}")
 
                 time.sleep(DEFAULT_TIME)
+                # there exists a list of element types to test, if we find the first one, we can break the loop and continue.
                 break
 
-    time.sleep(DEFAULT_TIME)
-    # Finalizar o driver
     driver.quit()
-
-    # "-----------------------------------------------------------------------------------------------------------------------------------"
 
     # Pegar todos os alertas da aplicação
     alerts = zap.alert.alerts()
-    print(f"Alerts: {len(alerts)}")
-    print(
-        alerts
-    )  # se não retornar alerta, lançar assert informando a possibilidade do scan passivo ter parado de funcionar
+    logging.info(f"Number of Alerts: {len(alerts)}")
+    # se não retornar alerta, lançar assert informando a possibilidade do scan passivo ter parado de funcionar
     # Alertas no momento que o post foi passado, isso inclui as aplicações que foram passadas de forma errada
     # Lembrar que pegar o ultimo ainda é o mais sensato (evitar casos que manda a credencial que queremos, mas de uma forma errada) tem que testar.
-    alert_autentication = []
-    #  Depurar para encontrar outras formas de identificação de autenticação pelo proxy (rodar pelo UI - getboo, gruyere)
-    for alert in alerts:
-        if alert["name"] == "Authentication Request Identified":
-            # alerta de autenticação
-            alert_autentication.append(alert)
-
-    # Pegar os ids de todas as mensagens de autenticação, ou de possiveis autenticação
-    messageId = []
-    for alert in alert_autentication:
-        messageId.append(alert["messageId"])
+    messageId = [alert["messageId"] for alert in alerts if alert["name"] == "Authentication Request Identified"]
 
     # verifica todos os alertas candidatos e filtra o que foi passado as credenciais de forma correta.
-    for id in messageId:
-        request_autenticated = zap.core.message(id)
-        # print(request_autenticated)
+    for message in messageId:
+        request_autenticated = zap.core.message(message)
         if autentication.check_credentials(
             request_autenticated["requestBody"],
             user.login,
             user.password,
         ):
-            print(
-                f'O response da aplicação no momento do login é:\n{request_autenticated["requestBody"]}'
+            logging.info(
+                f"The application's response during login: {request_autenticated["requestBody"]}"
             )
             break
 
@@ -257,8 +174,8 @@ def main(file_path):
 
     BASE_CONTEXT = "base_context/context_base.yaml"
 
-    params.Define_type_authentication(
-        zap, keywords.parameter_types_found, url_authentication, request_body
+    params.Define_authentication_type(
+        zap, parameter_types_found, url_authentication, request_body
     )
 
     yaml = YAML()
@@ -290,55 +207,67 @@ def main(file_path):
     with open(f"../shared_data/{OUTPUT}.yaml", "w") as file:
         yaml.dump(context, file)
 
-    print("Arquivo YAML modificado e salvo com sucesso !")
+    logging.info("YAML file saved successfully!")
+
+    output_yaml = f"../shared_data/{OUTPUT}.yaml"
+    logging.info(f" Yaml path: ",output_yaml)
+
+
+
+def wait_for_directory(directory_path):
+    directory_path = Path(directory_path)
+
+    if directory_path.exists():
+        if any(directory_path.iterdir()):
+            logging.info(f"Files found in directory: {directory_path}")
+            return
+        else:
+            logging.info(f"Empty Directory: {directory_path}")
+            return
+
+
+def spider_scan(driver, zap, user, urls_login, url_words, invalid_values_urls, s, firefox_options, struct_login):
+    # Primeira url será utilizada para realizar o crawler na página com o spider
+    driver.get(user.url[0])
 
     time.sleep(DEFAULT_TIME)
-
-    # mudar para o path da raiz
-    output_yaml = f"../shared_data/{OUTPUT}.yaml"
-    print(output_yaml)
-
-    scanid = zap.automation.run_plan(output_yaml, ZAP_API_KEY)
-
-    print(f"scanid plano = {scanid}")
-
-    while zap.automation.plan_progress(scanid)["finished"] == "":
-        time.sleep(1)
-
-    print(zap.automation.plan_progress(scanid)["finished"])
-
-    # rodar o spider autenticado
-    id_context = zap.context.context(user.context)["id"]
-    print(f"nome contexto = {user.context} id_context = {id_context}")
-
-    id_user = zap.users.users_list(id_context)[0]["id"]
-    print(f"id_user = {id_user}")
-
-    scanid = zap.spider.scan_as_user(id_context, id_user, apikey=ZAP_API_KEY)
-    print(f"scanid spider = {scanid}")
+    scanid = zap.spider.scan(user.url[0])
 
     while int(zap.spider.status(scanid)) < 100:
         # Loop until the spider has finished
-        print("Spider progress %: {}".format(zap.spider.status(scanid)))
+        logging.info("Spider progress %: {}".format(zap.spider.status(scanid)))
         time.sleep(DEFAULT_TIME)
 
-    time.sleep(1)
-    FILE_CONTEXT = f"{user.context}.context"
+    logging.info("Spider completed")
 
-    # mudar para o path da raiz
-    print(
-        zap.context.export_context(
-            user.context, f"/shared_data/{FILE_CONTEXT}", apikey=ZAP_API_KEY
-        )
-    )
+    driver.quit()
+
+    urls_found = zap.core.urls()
+
+    logging.info(urls_found)
+    general_results = urls_login.find_urls_login(urls_found, url_words)
+    logging.info(general_results)
+    evidences_urls_login = []
+    for url in general_results:
+        if not any(
+            re.search(pattern, url) for pattern in invalid_values_urls
+        ):  # match das palavras do array (regex group)
+            evidences_urls_login.append(url)
+
+    logging.info(evidences_urls_login)
+    for evidence in evidences_urls_login:
+        driver = webdriver.Firefox(service=s, options=firefox_options)
+        filtered_dict(driver, evidence, struct_login)
 
 
 if __name__ == "__main__":
-    wait_for_directory()
+    DIRECTORY_PATH = "/shared_data/input_config"
+    wait_for_directory(DIRECTORY_PATH)
     arq_config = os.listdir(DIRECTORY_PATH)
     for arq in arq_config:
         arq = os.path.join(DIRECTORY_PATH, arq)
         try:
             main(arq)
-        except:
-            pass
+        except Exception as e:
+            logging.error(f"An error occurred while processing the file {arq}: {e}")
+            raise RuntimeError(f"Failed to process the file {arq}") from e
