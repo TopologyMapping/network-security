b041f57008d6de9510b088a71e2b0f2190a37765
Bernnardo Seraphim <bernnardosbo@gmail.com>
2025-05-14 15:24:42 -0300

    formatting of reports concluded

--- voting_analysis/src/analysis/email_generator.py
+++ voting_analysis/src/analysis/email_generator.py
@@ -1,5 +1,21 @@
 from jinja2 import Environment, FileSystemLoader, Undefined
 
+
+def categorical_format(value):
+    MILD_UPPER_THRESHOLD = 1.5
+    MODERATE_UPPER_THRESHOLD = 2.5
+    SEVERE_UPPER_THRESHOLD = 3.5
+    
+    if value < MILD_UPPER_THRESHOLD:
+        return f"Mild({value})"
+    elif value < MODERATE_UPPER_THRESHOLD:
+        return f"Moderate({value})"
+    elif value < SEVERE_UPPER_THRESHOLD:
+        return f"Severe({value})"
+    else:
+        return f"Critical({value})"
+
+
 class SilentUndefined(Undefined):
     def _fail_with_undefined_error(self, *args, **kwargs):
         return ''  # Retorna string vazia em vez de erro
@@ -8,6 +24,8 @@ class SilentUndefined(Undefined):
 
 env = Environment(loader=FileSystemLoader("./templates"),
                   undefined=SilentUndefined)
+
+env.filters["categorical_format"] = categorical_format
 
 template = env.get_template("report.html")
 template_empty = env.get_template("empty_report.html")
@@ -20,14 +38,14 @@ TODO:
 
 """
 
-def generate_html(user_id, html_data):
+def generate_html(html_data, filename="default"):
         
     if html_data["is_empty"]:
         output = template_empty.render(html_data)
     else:
         output = template.render(html_data)
     
-    with open(f"../../data/htmls/{user_id}.html", "w") as f:
+    with open(f"../../data/htmls/{filename}.html", "w") as f:
         f.write(output)
 
 if __name__ == "__main__":
