f584e8fc76c769e137f3853ad258cdb438ef03e1
Italo Cunha <cunha@users.noreply.github.com>
2025-07-29 19:28:03 -0300

    feat(email scripts): scripts for sending a batch of emails using an html template (#65)
    Co-authored-by: Iago R <iago-r@outlook.com>

---
+++ scripts/email_sending_scripts/send_emails.py
@@ -0,0 +1,69 @@
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import time
+
+from settings import SENDER, SMTP_PASS, SMTP_PORT, SMTP_SERVER, SMTP_USER, USERS_FILE, REPORT_FILES
+
+def load_html_template(email):
+    path = os.path.join(REPORT_FILES, f"{email}.html")
+    with open(path, "r", encoding="utf-8") as file:
+        return file.read()
+
+def attach_inline_image(msg, image_path, content_id, filename):
+    if os.path.exists(image_path):
+        with open(image_path, "rb") as img_file:
+            img = MIMEImage(img_file.read())
+            img.add_header("Content-ID", f"<{content_id}>")
+            img.add_header("Content-Disposition", "inline", filename=filename)
+            msg.attach(img)
+    else:
+        print(f"Error finding the image: {image_path}")
+
+def send_email(server, name, email, has_vote):
+    email_body = load_html_template(email) # files are named with the "email.html" format
+    
+    msg = MIMEMultipart("related")
+    msg["From"] = SENDER
+    msg["To"] = "" # change to msg["To"] = email
+    msg["CC"] = "" # just for test
+    msg["Subject"] = "" # change to a subbject msg, like -> msg["Subject"] = "Bem-vindo(a) ao DefectDojo Crivo!"
+
+    alternative = MIMEMultipart("alternative") # safeguard
+    alternative.attach(MIMEText(email_body, "html"))
+    msg.attach(alternative)
+
+    if has_vote == "0": # flags to identify different e-mail templates (that contains or not images)
+        images = [
+            (os.path.join(REPORT_FILES, f"{email}.png"), "relatorio_pessoal", "relatorio.png"),
+            (os.path.join(REPORT_FILES, f"feat_importance_user_{email}_2x1.png"), "importancia", "importancia.png"),
+        ]
+
+        for path, cid, filename in images:
+            attach_inline_image(msg, path, cid, filename)
+    
+    email = ["", ""] # kind of a img list 
+
+    try:
+        server.sendmail(SENDER, email, msg.as_string())
+        print(f"Email sent to {name} <{email}>")
+    except Exception as e:
+        print(f"Error sending email to {name} <{email}>: {e}")
+
+def send_emails():
+    with open(USERS_FILE, "r", encoding="utf-8") as file:
+        try:
+            with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+                server.starttls()
+                server.login(SMTP_USER, SMTP_PASS)
+
+                for line in file:
+                    name, email, has_vote = map(str.strip, line.split(","))
+                    send_email(server, name, email, has_vote)
+                    time.sleep(25)
+        except Exception as e:
+            print(f"Error: {e}")
+        
+send_emails()
